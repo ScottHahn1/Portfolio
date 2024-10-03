bbc2fddd196b29e5f2cdc88cedbfa7f552546b09
ScottHahn1 <scotthahn2210@gmail.com>
2024-10-03 23:16:21 +0200

    added file

---
+++ src/components/Project.js
@@ -0,0 +1,36 @@
+import useFade from "../hooks/useFade";
+import { useRef } from "react";
+
+const Project = ({ title, technologiesUsed, overview, liveLink, githubLink, imgSrc, imgAlt }) => {
+    const ref = useRef();
+    const isVisible = useFade(ref);
+
+    return (
+        <div className={`project fade-in ${isVisible ? 'visible' : ''}`} ref={ref}>
+        <h3>{title}</h3>
+
+        <div className="project-technologies">
+          {technologiesUsed.map((tech, idx) => (
+                <p key={idx}>{tech}</p>
+          ))}
+        </div>
+
+        <p>
+          {overview}
+        </p>
+      
+        <div className="project-btns">
+          <a href={liveLink} target="_blank" rel="noreferrer">
+            <button>Live Demo</button>
+          </a>
+          <a href={githubLink} target="_blank" rel="noreferrer">
+            <button>See Code</button>
+          </a>
+        </div>
+
+        <img src={imgSrc} alt={imgAlt} />
+      </div>
+    )
+}
+
+export default Project;
