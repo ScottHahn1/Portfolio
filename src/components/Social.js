d6d4619cfc0bb16c5b4ccc8344031dd4d7e90a3d
ScottHahn1 <scotthahn2210@gmail.com>
2024-10-05 19:32:28 +0200

    split social links into seperate divs

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,30 +1,51 @@
 import LinkedInIcon from "./LinkedIn-Icon";
 import GithubIcon from "./Github-Icon";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import '../styles/Social.css';
+import EmailIcon from "./Email-Icon";
 
 const Social = () => {
 
     return (
         <div className='social-links'>
-            <a href='https://linkedin.com/in/scott-hahn' target='_blank' rel="noreferrer">
-                <div className='social' >
-                    <LinkedInIcon />
-                </div>
-            </a>
+        {
+            // <a href='https://linkedin.com/in/scott-hahn' target='_blank' rel="noreferrer">
+            //         <LinkedInIcon />
+            // </a>
             
-            <a href='https://github.com/ScottHahn1' target='_blank' rel="noreferrer">
-                <div className='social' >
-                    <GithubIcon />
-                </div>
-            </a>
+            // <a href='https://github.com/ScottHahn1' target='_blank' rel="noreferrer">
+            //         <GithubIcon />
+            // </a>
 
-            <a href='mailto:scotthahn2210@gmail.com'>
-                <div className='social'>
-                    <FontAwesomeIcon icon={faEnvelope} size='2x' style={{ padding: '.2rem', color: 'black' }} />
-                </div>
-            </a>
+            // <a href='mailto:scotthahn2210@gmail.com'>
+            //         <EmailIcon />
+            // </a>
+        }
+            <div>
+                <a href='https://linkedin.com/in/scott-hahn' target='_blank' rel="noreferrer">
+                    <div className="social-link">
+                        <p>LinkedIn</p>
+                        <LinkedInIcon />
+                    </div>
+                </a>
+            </div>
+
+            <div>
+                <a href='https://github.com/ScottHahn1' target='_blank' rel="noreferrer">
+                    <div className="social-link">
+                        <p>Github</p>
+                        <GithubIcon />
+                    </div>
+                </a>
+            </div>
+
+            <div>
+                <a href='mailto:scotthahn2210@gmail.com'>
+                    <div className="social-link">
+                        <p>E-mail</p>
+                        <EmailIcon />
+                    </div>
+                </a>
+            </div>
         </div>
     )
 }
