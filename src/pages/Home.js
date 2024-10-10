bbef682878096a65ae907aa1d14c7c11e0b08207
ScottHahn1 <scotthahn2210@gmail.com>
2024-10-11 00:12:21 +0200

    added Montserrat font

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,39 +2,24 @@ import '../styles/Home.css';
 import Projects from '../components/Projects';
 import About from '../components/About';
 import Contact from '../components/Contact';
-import { useState } from 'react';
-import { ThemeContext } from '../contexts/ThemeContext';
 import Social from '../components/Social';
-import { motion } from "framer-motion";
 
 const Home = () => {
-  const [darkTheme, setDarkTheme] = useState(false);
-
   return (
-    <ThemeContext.Provider value={{ darkTheme, setDarkTheme }}>
-      <main className='main'>
-        <motion.div className='main-content' id='home' style={{ backgroundColor: darkTheme && 'darkblue' }}
-         variants={{
-          hidden: { opacity: 0 },
-          visible: { opacity: 1 },
-        }}  
-          initial='hidden'
-          animate='visible'
-          transition={{ duration: 1 }}
-        >
-          <div className='intro'>
-            <h1>Scott Hahn</h1>
-            <h2>Full-Stack Web Developer</h2>
-          </div>
-          <Social />
-        </motion.div>
+    <main className='main'>
+      <div className='main-content' id='home'
+      >
+        <div className='intro montserrat'>
+          <h1>Scott Hahn</h1>
+          <h2>Full-Stack Web Developer</h2>
+        </div>
+        <Social />
+      </div>
 
-        <Projects />
-        <About />
-        <Contact />
-      </main>
-    </ThemeContext.Provider>
-
+      <Projects />
+      <About />
+      <Contact />
+    </main>
   )
 }
 
